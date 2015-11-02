f66ac4abc23cc3f6dcc7d5adda6bb42db092ab70
Dan Callahan <dan.callahan@gmail.com>
2015-11-02 10:50:07 -0600

    Initial Commit

---
+++ seashell.js
@@ -0,0 +1,22 @@
+// Thanks to:
+// - http://noisehack.com/generate-noise-web-audio-api/
+// - https://medium.com/web-audio/you-dont-need-that-scriptprocessor-61a836e28b42#.te1de1xc6
+
+var ctx = new AudioContext();
+var node = ctx.createBufferSource();
+var buffer = ctx.createBuffer(1, ctx.sampleRate, ctx.sampleRate);
+var data = buffer.getChannelData(0);
+
+var last = 0;
+for (var i = 0; i < ctx.sampleRate; i++) {
+  last = (last + (0.02 * (Math.random() * 2 - 1))) / 1.02;
+  data[i] = last * 3.5;
+}
+
+node.buffer = buffer;
+node.loop = true;
+node.start();
+
+window.addEventListener('userproximity', function(event) {
+  event.near ? node.connect(ctx.destination) : node.disconnect(ctx.destination);
+});
